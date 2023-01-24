f4953908a7d313a58de16945ebcbcda0714d18f2
Rutjake <77888584+Rutjake@users.noreply.github.com>
2023-01-24 21:14:57 +0200

    Javascript Funktiot

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
-function menuFunction() {
-    var x = document.getElementById("mobileMenu");
-    if (x.className === "mobileDown") {
-        x.className += " responsive";
-    } else {
-        x.className = "mobileDown";
-    }
+/* Lisätään teksti kun sivu ladataan */
+
+window.onload = function () {
+    etusivu();
+};
+
+function kuka() {
+    document.getElementById("mainPara").innerHTML = "<h3>Kuka?</h3><p>Tekstiä!</p>";
+}
+
+function etusivu() {
+    document.getElementById("mainPara").innerHTML = "<p>Osaan kaikkea ja enemmänkin, jos jaksan</p><p>Ju nou</p>";
+}
+
+function projektit() {
+    document.getElementById("mainPara").innerHTML = "<h3>Projektit</h3><p>Tekstiä!</p>"
+}
+
+function ota_yhteytta() {
+    document.getElementById("mainPara").innerHTML = "<h3>Ota Yhteyttä</h3><p>Tekstiä!</p>"
 }
