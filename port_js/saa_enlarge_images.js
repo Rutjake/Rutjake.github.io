0b16b44c03c399f954056cf278ee92d9b15c8355
Rutjake <jarnosanterimusic@gmail.com>
2023-09-18 20:44:27 +0300

    New Project: Sää Nyt. Date Update

--- port_js/saa_enlarge_images.js
+++ port_js/saa_enlarge_images.js
@@ -1,8 +1,28 @@
-// Code MatikkaPeli
+// Code Sää Tänään
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('Matik');
+var img = document.getElementById('saaEtu');
+var let = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function () {
+    modal.style.display = "block";
+    let.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+var modal = document.getElementById('myModal');
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById('tiedotMobi');
 var let = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function () {
@@ -21,7 +41,7 @@ span.onclick = function () {
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('MatikYhteen');
+var img = document.getElementById('tiedotWeb');
 var let = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function () {
@@ -40,26 +60,7 @@ span.onclick = function () {
 var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('MatikVaarin');
-var let = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
-    modal.style.display = "block";
-    let.src = this.src;
-    captionText.innerHTML = this.alt;
-}
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-var modal = document.getElementById('myModal');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('MatikKerto');
+var img = document.getElementById('taustaRov');
 var let = document.getElementById("img01");
 var captionText = document.getElementById("caption");
 img.onclick = function () {
